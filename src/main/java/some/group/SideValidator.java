e1a4dfd1474c83ac771be06b3ad2fd94ac41e781
methylene <theairspeedindicator@yahoo.com>
2012-11-28 21:18:12 +0000

    use futil 0.3.4

--- src/main/java/some/group/SideValidator.java
+++ src/main/java/some/group/SideValidator.java
@@ -2,9 +2,9 @@ package some.group;
 
 import static org.meth4j.futil.CommonKey.ERROR_TOO_LARGE;
 import static org.meth4j.futil.Message.errMesg;
-import static org.meth4j.futil.Validate.integer;
-import static org.meth4j.futil.Validate.positive;
-import static org.meth4j.futil.Validate.requiredString;
+import static org.meth4j.futil.Validate.notNegativeInt;
+import static org.meth4j.futil.Validate.notNullString;
+import static org.meth4j.futil.Validate.parseInt;
 import static some.group.SquareMethods.area;
 import static some.group.UnitOfLength.valueOf;
 
@@ -19,7 +19,7 @@ import javax.faces.validator.ValidatorException;
 public class SideValidator implements Validator {
 	
 	@Override public void validate(FacesContext context, UIComponent component, Object value) throws ValidatorException {
-		final Integer num = positive(integer(requiredString(value)));
+		final int num = notNegativeInt(parseInt(notNullString(value)));
 		final UISelectOne select = (UISelectOne) component.getAttributes().get("attr");
 		final UnitOfLength unit = valueOf((String) select.getSubmittedValue());
 		final double area = area(num, unit);
