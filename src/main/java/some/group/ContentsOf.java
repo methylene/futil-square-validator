0b2c658b153b08df776362aa6a18a1c72e84d94f
methylene <theairspeedindicator@yahoo.com>
2012-11-29 22:06:29 +0000

    use futil 0.3.5

--- src/main/java/some/group/ContentsOf.java
+++ src/main/java/some/group/ContentsOf.java
@@ -7,8 +7,9 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 import javax.faces.model.SelectItem;
 
-import org.meth4j.futil.Util;
+import org.meth4j.futil.IKey;
 
+import com.google.common.collect.ImmutableList;
 
 @ManagedBean
 @RequestScoped
@@ -17,11 +18,19 @@ public class ContentsOf {
 	private List<SelectItem> selectItems;
 
 	@PostConstruct public void init() {
-		selectItems = Util.toSelectItems(UnitOfLength.values());
+		selectItems = toSelectItems(UnitOfLength.values());
 	}
 
 	public List<SelectItem> getUnits() {
 		return selectItems;
 	}
 
+	private static List<SelectItem> toSelectItems(IKey[] kvs) {
+		final ImmutableList.Builder<SelectItem> unitBuilder = ImmutableList.builder();
+		for (final IKey kv : kvs) {
+			unitBuilder.add(new SelectItem(kv.getKey(), kv.getLabel()));
+		}
+		return unitBuilder.build();
+	}
+	
 }
